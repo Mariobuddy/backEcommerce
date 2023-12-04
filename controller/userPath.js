ca8ec31a565615d7f874a650aa044804363a7cc8
Mariobuddy <rohitbuatt9755@gmail.com>
2023-12-04 10:55:15 +0530

    host back 3

--- controller/userPath.js
+++ controller/userPath.js
@@ -105,11 +105,11 @@ const Login = async (req, res, next) => {
 
     const token = await userData.generateAuthToken();
 
-    res.cookie("jwt", token, {
-      secure: true,
-      expires: new Date(Date.now() + 86400000),
-      sameSite:"none"
-    });
+    // res.cookie("jwt", token, {
+    //   secure: true,
+    //   expires: new Date(Date.now() + 86400000),
+    //   sameSite:"none"
+    // });
     return res.status(200).json({ sucess: true, tokens:token });
   } catch (error) {
     return next(new customError("Internal server error", 500, "error"));
